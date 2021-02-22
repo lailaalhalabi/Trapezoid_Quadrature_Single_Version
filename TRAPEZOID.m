4b1a12e62393c9e46e5af1ca3cdd1ab1be03673b
Laila Alhalabi <70773705+lailaalhalabi@users.noreply.github.com>
2021-02-22 17:46:48 -0500

    Add files via upload

---
+++ TRAPEZOID.m
@@ -0,0 +1,15 @@
+function I = TRAPEZOID(f,a,b)
+  
+  #trapezoid quadrature formula (single version).
+  
+  format long
+  x = linspace(a,b,1);
+  h = b-a;
+  fprintf('\nLONG FORMAT\n')
+  h*(sum(f(x)))/2
+  fprintf('SHORT FORMAT\n')
+  format short
+  h*(sum(f(x)))/2
+  fprintf('\n')
+  
+endfunction
